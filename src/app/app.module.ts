9c902f86b0d342b32deabdf983861376adc26698
TJWaseem511 <tjwasim@gmail.com>
2019-03-19 22:31:17 +0530

    Working app with header, major components & routes setup 4 files modified, 4 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { DeploymentsComponent } from './deployments/deployments.component';
+import { SessionsComponent } from './sessions/sessions.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+const appRoutes: Routes = [
+  {path:'', redirectTo: 'deployments', pathMatch: 'full'},
+  {path: 'deployments', component: DeploymentsComponent},
+  {path: 'sessions', component: SessionsComponent}
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, 
+                  FormsModule, 
+                  RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, DeploymentsComponent, SessionsComponent, WelcomeComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
